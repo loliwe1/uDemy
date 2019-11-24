ae7f73dc421bb2426f98251c98df560f1b7eb912
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-25 00:07:13 +0400

    lesson2

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ let time = prompt("Введите дату в формате YYYY-MM-DD");
 const appData = {
     bugdet: money,
     timeData: time,
-    expenses:{},
-    optionalExpenses:{},
-    income:[],
-    savings: false 
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
-for(let i=0; i<2; i++){
+for (let i = 0; i < 2; i++) {
     let q1 = prompt('Введите обязательную статью расходов в этом месяце');
     let q2 = prompt('Во сколько обойдется?');
 
-    appData.expenses[q1] = q2;
+    if((typeof (q1) )!== 'string'|| q1=== '' || q1=== null ||q2=== ''|| q2=== null || isNaN(q2)|| q1.length >50){
+        i--;
+    }else{
+        appData.expenses[q1] = q2;
+    }
+
+
 };
 
-alert(`Ваш бюджет на 1 день равен ${appData.bugdet/30} рублей`);
+appData.moneyPerDay = appData.bugdet/30;
+alert(`Ваш бюджет на 1 день равен ${appData.moneyPerDay} рублей`);
 
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}

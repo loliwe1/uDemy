65e2bf8f3a969ebee226a5cc8369f5b3b65e3043
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-27 01:15:30 +0400

    lesson12 ES6 practice

--- lesson12/script.js
+++ lesson12/script.js
@@ -1,25 +1,22 @@
-'use strict'
 
-class Option{
-    constructor(height, width, bg, fontSize, textAlign){
-        this.height = height;
-        this.width = width;
-        this.bg = bg;
-        this.fontSize = fontSize;
-        this.textAlign = textAlign;
-    }
-    createDiv(text){
-      let div =   document.createElement('div');
-      div.style.cssText = `height: ${this.height}px;
-                           width: ${this.width}px;
-                           background: ${this.bg};
-                           font-size: ${this.fontSize}px;
-                           text-align: ${this.textAlign};`
-        div.textContent = text;
-        document.body.append(div);
-    }
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
 
+	createDiv(text) {
+        let elem = document.createElement('div');
+        elem.textContent = text;
+		document.body.appendChild(elem);
+		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = param;
+	}
 }
 
-let div = new Option(200,200,'red',20, 'center');
-div.createDiv('Hello world!');
+const item = new Options(300, 350, "red", 20, "center");
+
+item.createDiv('Hello wordl!!!');

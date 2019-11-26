207cdd0be35b8321d68a46545d41d875dacd84e0
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-26 19:08:12 +0400

    lesson 10 (timer)

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict'
-
+    // TABS----------------------------------------------------------------
     let tab = document.querySelectorAll('.info-header-tab');
     let info = document.querySelector('.info-header');
     let tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,6 +33,62 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     })
+
+    // TIMER--------------------------------------------------------------
+    let deadline = '2019-11-27';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+
+        let seconds = Math.floor(t / 1000 % 60);
+        let minutes = Math.floor(t / 1000 / 60 % 60);
+        let hours = Math.floor(t / 1000 / 60 / 60);
+        if(t<= 0) {
+            seconds = '00';
+            minutes = '00';
+            hours = '00';
+        }
+        if(seconds < 10){
+            seconds = '0' + seconds;
+        }
+        if(hours < 10){
+            hours = '0' + hours;
+        }
+        if(minutes < 10){
+            minutes = '0' + minutes;
+        }
+       
+        
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            "seconds": seconds
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total<=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 
 
 
